7e6008a393a03a2160927c6d74268df467f06e03
MarvinCorro <corro.marvin@gmail.com>
2018-10-27 15:02:30 -0700

    changed routing

--- public/src/app/app-routing.module.ts
+++ public/src/app/app-routing.module.ts
@@ -1,11 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GlobeComponent } from './globe/globe.component';
+import { FeedComponent } from './feed/feed.component';
+import { TopContainerComponent } from './top-container/top-container.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home', component: GlobeComponent },
-  { path: '**', redirectTo: '/home'}
+  { path: 'app',
+    children: [
+        {path: 'globe', component: GlobeComponent},
+        {path: 'feed', component: FeedComponent, outlet: 'twitter-feed'},
+    ],
+  },
+  { path: '**', redirectTo: "/app/(globe//twitter-feed:feed)"}
 ];
 
 @NgModule({
